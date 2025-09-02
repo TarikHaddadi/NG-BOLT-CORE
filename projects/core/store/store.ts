8d3c40ed9290285fcf63f58565f7a34a20f72e2c
HADDADI Tarik <tarik.haddadi@proximus.lu>
2025-09-02 13:20:37 +0200

    chore(docs): store behaviour

--- projects/core/store/store.ts
+++ projects/core/store/store.ts
@@ -1,3 +1,4 @@
+import { isDevMode } from '@angular/core';
 import { provideEffects } from '@ngrx/effects';
 import { ActionReducer, MetaReducer, provideStore } from '@ngrx/store';
 import { localStorageSync } from 'ngrx-store-localstorage';
@@ -15,10 +16,25 @@ export const metaReducers: MetaReducer<AppState>[] = [localStorageSyncReducer];
 export const provideAppStore = () => [
   provideStore(AppReducers, {
     metaReducers,
-    runtimeChecks: {
-      strictActionImmutability: true,
-      strictStateImmutability: true,
-    },
+    runtimeChecks: isDevMode() ? devRuntimeChecks : prodRuntimeChecks,
   }),
   provideEffects(AppEffects),
 ];
+
+const devRuntimeChecks = {
+  strictActionImmutability: true,
+  strictStateImmutability: true,
+  strictActionSerializability: true,
+  strictStateSerializability: true,
+  strictActionWithinNgZone: true,
+  strictActionTypeUniqueness: true,
+};
+
+const prodRuntimeChecks = {
+  strictActionImmutability: true,
+  strictStateImmutability: true,
+  strictActionSerializability: false,
+  strictStateSerializability: false,
+  strictActionWithinNgZone: false,
+  strictActionTypeUniqueness: false,
+};
